17b18b13892c3f346e36bbc2e336ffb1272518a8
Marcus <faderl.marcus@gmail.com>
2020-04-25 13:48:15 +0200

    first commit

---
+++ build/precache-manifest.2d2df75ee391dfbe4e9263a8c844afab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9683e6ca1e4d8e6972e635245c0b94f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b512008ffd2688653db1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f974d6f42a555980b0c6",
+    "url": "/static/js/2.6b709532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b709532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b512008ffd2688653db1",
+    "url": "/static/js/main.24d9a685.chunk.js"
+  },
+  {
+    "revision": "bf7f2511e7724b9bd243",
+    "url": "/static/js/runtime-main.d3d45e0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
